8bd9de132871ab51d48c1895c17ca8d6f136678f
Vincent CARITEY <vincent.caritey@kisss.fr>
2019-05-29 00:57:56 +0200

    Fixed export issue

--- src/index.js
+++ src/index.js
@@ -5,15 +5,20 @@
 
 import { UserManager } from 'oidc-client';
 import { PluginConfiguration } from './plugin-configuration';
+import { Oauth2Interceptor } from './oauth2-interceptor';
+import { OpenidRouting } from './openid-routing';
+import { Connection } from './connection';
 
 /**
  * Configures the plugin.
  * @param {FrameworkConfiguration} aurelia - the aurelia framework configuration
  * @param {*} pluginCallback - the plugin callback
  */
-export function configure(aurelia, pluginCallback) {
+function configure(aurelia, pluginCallback) {
   const pluginConfiguration = pluginCallback();
   aurelia.container.registerInstance(UserManager, new UserManager(pluginConfiguration.userManagerSettings));
   aurelia.container.registerInstance(PluginConfiguration, pluginConfiguration);
   aurelia.container.registerInstance(Window, window);
 }
+
+export { configure, Oauth2Interceptor, OpenidRouting, Connection };
